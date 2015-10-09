8c4e0e5d89c9d21a20132798e7b3d68d76e46fd1
jadock913 <tsukamoto@nalab.jp>
2015-10-09 17:26:03 +0900

    test

--- testDir1/TestMain2.java
+++ testDir1/TestMain2.java
@@ -1,7 +1,6 @@
 
-public class TestMain {
+public class TestMain2 {
 	public static void main(String[] args) {
 		System.out.println("hello!");
-		System.out.println("hello2!");
 	}
 }
--- testDir1/TestMain3.java
+++ testDir1/TestMain3.java
@@ -1,7 +1,6 @@
 
-public class TestMain {
+public class TestMain3 {
 	public static void main(String[] args) {
 		System.out.println("hello!");
-		System.out.println("hello2!");
 	}
 }
