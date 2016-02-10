41ec682fafc85a0fe6468c0585d257269acc1896
jadock913 <tsukamoto@nalab.jp>
2016-02-10 20:24:45 +0900

    H2削除

--- testDir1/TestMain.java
+++ testDir1/TestMain.java
@@ -2,7 +2,7 @@
 public class TestMain {
 	public static void main(String[] args) {
 		System.out.println("hello!");
-		System.out.println("hello2!");
+
 		System.out.println("hello3!");
 		System.out.println("hello4!");
 		System.out.println("hello5!");
