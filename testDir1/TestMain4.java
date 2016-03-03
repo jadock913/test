4a59aca36ed5e232dc7e4344f9c6e27bea4eb969
jadock913 <tsukamoto@nalab.jp>
2016-03-03 11:57:17 +0900

    test commit です。

--- testDir1/TestMain4.java
+++ testDir1/TestMain4.java
@@ -3,5 +3,6 @@ public class TestMain3 {
 	public static void main(String[] args) {
 		System.out.println("hello!");
 		System.out.println("hello44444!");
+		System.out.println("hello44444A!");
 	}
 }
